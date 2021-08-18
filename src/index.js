5e17b28ffa0cdb2ecf0ca0af8b536e865b1df313
Scott Nasman <scottnasman@gmail.com>
2021-08-18 08:44:28 -0600

    Styling added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import { loadNav } from './nav.js'
 import { loadHome } from './home.js'
 import { loadMenu } from './menu.js'
 import { loadContact } from './contact.js'
+import './style.css';
 
+loadNav();
 loadHome();
 loadMenu();
 loadContact();
